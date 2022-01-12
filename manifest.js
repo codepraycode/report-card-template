7187a9132311f2a6736603bd5365d81cf539de4a
Adejare Omomofe <omomofeadejare257@gmail.com>
2022-01-12 10:23:05 +0100

    initial commit

---
+++ manifest.js
@@ -0,0 +1,62 @@
+//jshint esversion:8
+const STUDENT_DATA = {
+  name: "John Doe",
+  major: "Computer Engineering",
+  matricNumber: "CPE/102/3A2",
+  faculty: "Engineering",
+  level: "200",
+  term: "2nd Semester",
+  subjects: [
+    {
+      courseCode: "EMA 281",
+      courseTitle: "Engineering Mathematics II",
+      score: 76,
+      grade: "A"
+    },
+    {
+      courseCode:"CHE 222",
+      courseTitle: "Material Science",
+      score: 69,
+      grade: "B"
+    },
+    {
+      courseCode: "MEE 252",
+      courseTitle: "Fundamental Fluid Mechanics",
+      score: 70,
+      grade: "A"
+    },
+    {
+      courseCode: "MEE 222",
+      courseTitle: "Engine Drawing II",
+      score: 60,
+      grade: "B"
+    },
+     {
+      courseCode: "EEE 212",
+      courseTitle: "Electrical Engineering II",
+      score: 65,
+      grade: "B"
+    },
+    {
+      courseCode: "CVE 212",
+      courseTitle: "Manufacturing Technology",
+      score: 70,
+      grade: "A"
+    },
+    {
+      courseCode: "MEE 262",
+      courseTitle: "Fundamental of Thermodynamics",
+      score: 73,
+      grade: "A"
+    },
+    {
+      courseCode: "ELE 201",
+      courseTitle: "Laboratory/Workshop Practice",
+      score: 79,
+      grade: "A"
+    }
+
+  ]
+};
+
+module.exports = STUDENT_DATA;
---
+++ server.js
@@ -0,0 +1,26 @@
+//jshint esversion:8
+
+const express = require('express');
+const manifest = require('./manifest.js');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    manifest:manifest
+  });
+});
+
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+
+
+app.listen(port, function() {
+  console.log('Server has started Successfully!.');
+});
