836b8a2af1aaee727d38fb31e9975a75b7951fef
Precious Olusola <preciousolusola16@gmail.com>
2022-01-17 14:13:57 +0100

    Updated the report_manifest, changed the server port to 3020

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ app.use(express.static("public"));
 
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    manifest:manifest
-  });
+    res.render('index', {
+        manifest: manifest
+    });
 });
 
 let port = process.env.PORT;
-if (port == null || port == ""){
-  port = 3000;
+if (port == null || port == "") {
+    port = 3020;
 }
 
 
 
 app.listen(port, function() {
-  console.log('Server has started Successfully!.');
+    console.log('Server has started Successfully!.');
 });
